5ff3b5d809f56680e2ce08ea66db0e1a568cc91b
SihamELBOUBAKRI <155733015+SihamELBOUBAKRI@users.noreply.github.com>
2025-01-02 15:39:35 +0100

    commit

--- library-back-end/routes/api.php
+++ library-back-end/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Authors
+Route::get('/authors', [AuthorController::class,'index']);
+//books-to-rent
+
+//books-to-sell
+
+//categories
+
+//customers
+
+//orders
+
+//transactions
+
+//wishlist
+
+//active-rental-books
+
+//purchases-books
